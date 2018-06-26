acdb82750e035e93f8e180b46563713dcf0bc1ec
shahriar hossain <shahriar_cse@live.com>
2018-06-26 19:22:57 +0600

    creating functional based component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Component/Person/Person'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
           <h1>Hi! This is from React app</h1>
+          <Person />
       </div>
     );
   }
---
+++ src/Component/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const person = ()=>{
+    return <p>I am a person. Lets generate a dynamic content Random Number {Math.random()} </p>
+}
+
+export default person;
