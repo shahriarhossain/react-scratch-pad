58d594cd9cc2d4f97a2c03b26bbe7b2d438b6726
shahriar hossain <shahriar_cse@live.com>
2018-06-27 20:14:07 +0600

    adding custom style

--- src/Component/Person/Person.js
+++ src/Component/Person/Person.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import Person from './Person.css';
 
 const person = (props)=>{
     return (
-        <div>
+        <div className="Person">
             <p><b onClick={props.click}>{props.children}</b> I am {props.name}. Lets generate a dynamic content. Random Number {Math.random()} </p>
             <input type="text" onChange = {props.setNewName}/>
         </div>
