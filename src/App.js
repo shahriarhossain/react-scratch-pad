1f168536ccd943338366b8e2732efd62b7edb336
shahriar hossain <shahriar_cse@live.com>
2018-06-27 15:00:19 +0600

    working with state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,24 @@ import './App.css';
 import Person from './Component/Person/Person'
 
 class App extends Component {
+  state = {
+    people: [
+      {name: 'shahriar'},
+      {name: 'alison'},
+      {name: 'spancer'}
+    ]
+  }
+
+  onChangeNameHandler = ()=>{
+    this.setState({
+      people: [
+        {name: 'hanna'},
+        {name: 'charllot'},
+        {name: 'spancer'}
+      ]
+    })
+  }
+
   showSomeAlert = ()=>{
     alert("Hello! I am an alert box!");
   }
@@ -10,10 +28,11 @@ class App extends Component {
     return (
       <div className="App">
           <h1>Hi! This is from React app</h1>
-          <Person name='shahriar' />
-          <Person name='alison' />
-          <Person name='spancer'>Team Leader</Person>
+          <Person name={this.state.people[0].name} />
+          <Person name={this.state.people[1].name} />
+          <Person name={this.state.people[2].name} click={this.onChangeNameHandler}>Team Leader</Person>
           <button onClick={this.showSomeAlert}>Click Me</button>
+          <button onClick={this.onChangeNameHandler}>Change Me</button>
       </div>
     );
   }
