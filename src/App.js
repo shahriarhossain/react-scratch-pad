101377bce3a61b2fc4ccf33a7dc859ff5043ecef
shahriar hossain <shahriar_cse@live.com>
2018-06-28 13:07:19 +0600

    rendering content conditionally

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
       {name: 'shahriar'},
       {name: 'alison'},
       {name: 'spancer'}
-    ]
+    ],
+    showList : true
   }
 
   onChangeNameHandler = ()=>{
@@ -34,16 +35,27 @@ class App extends Component {
   showSomeAlert = (msg)=>{
     alert(msg);
   }
+
+  onToggleChangeHandler = ()=>{
+    const currentStatus = this.state.showList;
+    this.setState({showList : !currentStatus});
+  }
+
   render() {
     return (
       <div className="App">
           <h1>Hi! This is from React app</h1>
-          <Person name={this.state.people[0].name}  />
-          <Person name={this.state.people[1].name} setNewName={this.onChangeSetNewNameHandler} />
-          <Person name={this.state.people[2].name} click={this.onChangeNameHandler}>Team Leader</Person>
+          {this.state.showList ?
+            <div>
+              <Person name={this.state.people[0].name}  />
+              <Person name={this.state.people[1].name} setNewName={this.onChangeSetNewNameHandler} />
+              <Person name={this.state.people[2].name} click={this.onChangeNameHandler}>Team Leader</Person>
+            </div> : null
+          }
           <button onClick={this.showSomeAlert.bind(this, 'The recommended way')}>Click Me 1</button>
           <button onClick={()=>this.showSomeAlert('Not the recommended way to bind, has performance issue ')}>Click Me 2</button>
           <button onClick={this.onChangeNameHandler}>Change Me</button>
+          <button onClick={this.onToggleChangeHandler}>Toggle List</button>
       </div>
     );
   }
